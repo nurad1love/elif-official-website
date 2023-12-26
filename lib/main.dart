342762046def4867af863d79495e5fb6a54e009f
VortexMMA <vortexhumanoid40@gmail.com>
2023-12-26 13:40:47 +0400

    start commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const EliWebfSite());
+
+class EliWebfSite extends StatelessWidget {
+  const EliWebfSite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

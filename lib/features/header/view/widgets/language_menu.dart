76bb0ec9104ce26036d8bf9b4c61b6c4c8a18206
mancolo77 <64766161+mancolo77@users.noreply.github.com>
2023-12-27 15:56:35 +0600

    Create language_menu.dart

---
+++ lib/features/header/view/widgets/language_menu.dart
@@ -0,0 +1,44 @@
+  import 'package:flutter/material.dart';
+
+void showLanguageMenu(BuildContext context) {
+    final RenderBox appBarRenderBox = context.findRenderObject() as RenderBox;
+    final RenderBox overlayRenderBox = Overlay.of(context).context.findRenderObject() as RenderBox;
+
+    showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(
+        appBarRenderBox.size.width - 100,
+        appBarRenderBox.size.height,
+        0,
+        0,
+      ),
+      items: [
+        const PopupMenuItem(
+          value: 'Ru',
+          child: Row(
+            children: [
+              Icon(Icons.language, color: Colors.blue),
+              SizedBox(width: 8),
+              Text('Ru'),
+            ],
+          ),
+        ),
+        const PopupMenuItem(
+          value: 'En',
+          child: Row(
+            children: [
+              Icon(Icons.language, color: Colors.blue),
+              SizedBox(width: 8),
+              Text('En'),
+            ],
+          ),
+        ),
+      ],
+      elevation: 8.0,
+    ).then((value) {
+      if (value != null) {
+        // Обработка выбора языка
+        print('Выбран язык: $value');
+      }
+    });
+  }
